777352dbae4cf29658150068daec9ee43b2c6255
mateusz58 <matp321@gmail.com>
2019-09-24 02:49:02 +0200

    ImmutableClassForumUser

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,13 @@
+package com.kodilla.stream.immutable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ForumUser {
+
+    private final String username;
+    private final String realname;
+
+}
