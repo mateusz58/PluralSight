077112ffd4d73c1ba554e2256433586f826cf3bd
zlp <matp321@gmail.com>
2019-12-14 23:34:54 +0100

    Added nosql

--- tddspringboot-sql/src/test/java/com/globomantics/productservice/integration/ProductServiceIntegrationTest.java
+++ tddspringboot-sql/src/test/java/com/globomantics/productservice/integration/ProductServiceIntegrationTest.java
@@ -2,9 +2,11 @@ package com.globomantics.productservice.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.connection.ConnectionHolder;
-import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.junit5.DBUnitExtension;
 import com.globomantics.productservice.model.Product;
+import com.globomantics.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +21,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.sql.DataSource;
 
+import java.sql.SQLException;
+
+import static com.globomantics.productservice.TestingSqlQueries.CREATE_TABLE;
+import static com.globomantics.productservice.TestingSqlQueries.DROP_TABLE;
 import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -35,6 +41,25 @@ class ProductServiceIntegrationTest {
     @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
+    private ProductRepository repository;
+
+    @BeforeEach
+    void init() throws SQLException {
+        dataSource.getConnection().prepareStatement(CREATE_TABLE).executeUpdate();
+        dataSource.getConnection().prepareStatement("INSERT INTO products(name,quantity,version) VALUES ('Product 1', 10,1)").executeUpdate();
+        dataSource.getConnection().prepareStatement("INSERT INTO products(name,quantity,version) VALUES ('Product 2', 5,2);").executeUpdate();
+    }
+
+    @AfterEach
+    void finish() throws SQLException {
+        dataSource.getConnection().prepareStatement("TRUNCATE products").execute();
+        dataSource.getConnection().prepareStatement(DROP_TABLE).execute();
+    }
+
     static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
@@ -50,7 +75,7 @@ class ProductServiceIntegrationTest {
 
     @Test
     @DisplayName("GET /product/1 - Found")
-    @DataSet("products.yml")
+    
     void testGetProductByIdFound() throws Exception {
         // Execute the GET request
         mockMvc.perform(get("/product/{id}", 1))
@@ -64,15 +89,11 @@ class ProductServiceIntegrationTest {
             .andExpect(header().string(HttpHeaders.LOCATION, "/product/1"))
 
             // Validate the returned fields
-            .andExpect(jsonPath("$.id", is(1)))
-            .andExpect(jsonPath("$.name", is("Product 1")))
-            .andExpect(jsonPath("$.quantity", is(10)))
-            .andExpect(jsonPath("$.version", is(1)));
+            .andExpect(content().json(mapper.writeValueAsString(repository.findById(1))));
     }
 
     @Test
     @DisplayName("GET /product/99 - Not Found")
-    @DataSet("products.yml")
     void testGetProductByIdNotFound() throws Exception {
         // Execute the GET request
         mockMvc.perform(get("/product/{id}", 99))
@@ -83,7 +104,6 @@ class ProductServiceIntegrationTest {
 
     @Test
     @DisplayName("POST /product - Success")
-    @DataSet("products.yml")
     void testCreateProduct() throws Exception {
         // Setup product to create
         Product postProduct = new Product("Product Name", 10);
@@ -109,7 +129,6 @@ class ProductServiceIntegrationTest {
 
     @Test
     @DisplayName("PUT /product/2 - Success")
-    @DataSet("products.yml")
     void testProductPutSuccess() throws Exception {
         // Setup product to update
         Product putProduct = new Product("Product 2 Updated", 10);
@@ -136,7 +155,6 @@ class ProductServiceIntegrationTest {
 
     @Test
     @DisplayName("PUT /product/1 - Version Mismatch")
-    @DataSet("products.yml")
     void testProductPutVersionMismatch() throws Exception {
         // Setup product to update
         Product putProduct = new Product("Product Name", 10);
@@ -152,7 +170,6 @@ class ProductServiceIntegrationTest {
 
     @Test
     @DisplayName("PUT /product/99 - Not Found")
-    @DataSet("products.yml")
     void testProductPutNotFound() throws Exception {
         // Setup product to update
         Product putProduct = new Product("Product Name", 10);
@@ -165,10 +182,9 @@ class ProductServiceIntegrationTest {
             // Validate the response code and content type
             .andExpect(status().isNotFound());
     }
-
     @Test
     @DisplayName("DELETE /product/1 - Success")
-    @DataSet("products.yml")
+    
     void testProductDeleteSuccess() throws Exception {
         // Execute our DELETE request
         mockMvc.perform(delete("/product/{id}", 1))
@@ -177,7 +193,6 @@ class ProductServiceIntegrationTest {
 
     @Test
     @DisplayName("DELETE /product/99 - Not Found")
-    @DataSet("products.yml")
     void testProductDeleteNotFound() throws Exception {
         // Execute our DELETE request
         mockMvc.perform(delete("/product/{id}", 99))
