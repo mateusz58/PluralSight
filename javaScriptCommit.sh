f36237886ccda52576542b733ffebbcc6e02917a
mateusz58 <matp321@gmail.com>
2019-12-31 04:42:46 +0100

    Testing

---
+++ javaScriptCommit.sh
@@ -0,0 +1,5 @@
+git add . && git commit -am "$1" && git push origin master
+
+cp ReactTraining/src/javaScriptsTraining.js "/media/sdb1/Google drive/Projects_programming/JavaScript/JavaScriptInterviewQuestions/"
+
+cd  "/media/sdb1/Google drive/Projects_programming/JavaScript/JavaScriptInterviewQuestions/"  && git add . &&  git commit -am "$1" && git push origin master
